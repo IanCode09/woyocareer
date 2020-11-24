d6bdea339982614729edb1fe298c70f020d70f3d
IanCode09 <ianlombu@protonmail.com>
2020-11-24 14:53:47 +0700

    Hero Component

---
+++ frontend/src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Jumbotron, Button } from 'react-bootstrap'
+
+const Hero = () => {
+    return (
+        <div>
+            <Jumbotron className='text-center'>
+                <h4 className='text-light'>Selamat Datang</h4>
+                <h1>SHARED SERVICE OPERATION</h1>
+                <h1>WOYO GROUP</h1>
+                
+                <Button variant="primary">Temukan Loker</Button>
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default Hero
